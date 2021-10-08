db924d1dbffc88166116b819b2c9b74fd1087722
Jose V Sebastian <josevseb99@gmail.com>
2021-10-08 23:21:14 +0530

    complete challenge

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [Time, setTime] = useState("TIME");
+
+  function getTime() {
+    setTime(new Date().toLocaleTimeString("en-GB"));
+  }
+
+  setInterval(getTime, 1000);
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{Time}</h1>
+      <button onClick={getTime}>Get Time</button>
     </div>
   );
 }
